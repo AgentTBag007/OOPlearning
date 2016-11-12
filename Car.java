87a522ee26ae5d38080388c0ebca487918c307d8
AgentTBag007 <bagirov.timur@gmail.com>
2016-11-12 15:02:46 +0300

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,33 @@
+package ru.samsung.itschool.student3.ru;
+
+public class Car {
+
+
+    public int noOfWheels;
+    private int power;
+
+    public Car(){}
+    // перегрузка
+    public Car(int noOfWheels, int power){
+        this.noOfWheels = noOfWheels;
+        this.power = power;
+    }
+
+    public int getNoOfWheels() {
+        return noOfWheels;
+    }
+
+    public void setNoOfWheels(int noOfWheels) {
+        this.noOfWheels = noOfWheels;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+
+}
